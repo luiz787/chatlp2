edf0f76c04cac4a36cde02ee96455d12beb0ab84
luiz787 <diasbertoluiz@gmail.com>
2017-10-04 11:05:45 -0300

    Conserta sql

--- Chat/src/sql/chat.sql
+++ Chat/src/sql/chat.sql
@@ -1,6 +1,6 @@
-﻿/*
-Created: 02/10/2017
-Modified: 02/10/2017
+/*
+Created: 04/10/2017
+Modified: 04/10/2017
 Model: MySQL 5.6
 Database: MySQL 5.6
 */
@@ -8,12 +8,23 @@ Database: MySQL 5.6
 
 -- Create tables section -------------------------------------------------
 
+-- Table Sala
+
+CREATE TABLE `Sala`
+(
+  `nom_sala` Char(20) NOT NULL
+)
+;
+
+ALTER TABLE `Sala` ADD  PRIMARY KEY (`nom_sala`)
+;
+
 -- Table Usuario
 
 CREATE TABLE `Usuario`
 (
-  `nom_usuario` Text NOT NULL,
-  `nom_sala` Text
+  `nom_usuario` Char(30) NOT NULL,
+  `nom_sala` Char(20)
 )
 ;
 
@@ -23,33 +34,24 @@ CREATE INDEX `IX_Relationship1` ON `Usuario` (`nom_sala`)
 ALTER TABLE `Usuario` ADD  PRIMARY KEY (`nom_usuario`)
 ;
 
--- Table Sala
-
-CREATE TABLE `Sala`
-(
-  `nom_sala` Text NOT NULL
-)
-;
-
-ALTER TABLE `Sala` ADD  PRIMARY KEY (`nom_sala`)
-;
-
 -- Table Mensagem
 
 CREATE TABLE `Mensagem`
 (
-  `cod_mensagem` Int NOT NULL AUTO_INCREMENT,
-  `des_conteudo` Text,
-  `nom_usuario` Text,
-  `nom_sala` Text,
-  PRIMARY KEY (`cod_mensagem`)
+  `cod_mensagem` Int NOT NULL,
+  `nom_sala` Char(20),
+  `nom_usuario` Char(30),
+  `des_conteudo` Text NOT NULL
 )
 ;
 
-CREATE INDEX `IX_Relationship2` ON `Mensagem` (`nom_usuario`)
+CREATE INDEX `IX_Relationship2` ON `Mensagem` (`nom_sala`)
 ;
 
-CREATE INDEX `IX_Relationship3` ON `Mensagem` (`nom_sala`)
+CREATE INDEX `IX_Relationship3` ON `Mensagem` (`nom_usuario`)
+;
+
+ALTER TABLE `Mensagem` ADD  PRIMARY KEY (`cod_mensagem`)
 ;
 
 -- Create relationships section ------------------------------------------------- 
@@ -57,9 +59,8 @@ CREATE INDEX `IX_Relationship3` ON `Mensagem` (`nom_sala`)
 ALTER TABLE `Usuario` ADD CONSTRAINT `Relationship1` FOREIGN KEY (`nom_sala`) REFERENCES `Sala` (`nom_sala`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ;
 
-ALTER TABLE `Mensagem` ADD CONSTRAINT `Relationship2` FOREIGN KEY (`nom_usuario`) REFERENCES `Usuario` (`nom_usuario`) ON DELETE RESTRICT ON UPDATE RESTRICT
+ALTER TABLE `Mensagem` ADD CONSTRAINT `Relationship2` FOREIGN KEY (`nom_sala`) REFERENCES `Sala` (`nom_sala`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ;
 
-ALTER TABLE `Mensagem` ADD CONSTRAINT `Relationship3` FOREIGN KEY (`nom_sala`) REFERENCES `Sala` (`nom_sala`) ON DELETE RESTRICT ON UPDATE RESTRICT
+ALTER TABLE `Mensagem` ADD CONSTRAINT `Relationship3` FOREIGN KEY (`nom_usuario`) REFERENCES `Usuario` (`nom_usuario`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ;
-
