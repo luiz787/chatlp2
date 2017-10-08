aaac16412d09b80d2d621eb4203eab7c105d9e54
RafaelGSNeves <rafaelneves.99@hotmail.com>
2017-10-08 19:36:34 -0300

    termina controlador

--- Chat/src/controller/ChatController.java
+++ Chat/src/controller/ChatController.java
@@ -20,8 +20,6 @@ import domain.Usuario;
 import exception.BusinessException;
 import exception.PersistenceException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -31,6 +29,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
@@ -47,7 +46,7 @@ import serviceimpl.ManterUsuarioImpl;
  *
  * @author Luiz
  */
-public class ChatController implements Initializable {
+public class ChatController implements Initializable   {
 
     @FXML
     private Button botaoEntrar;
@@ -59,12 +58,7 @@ public class ChatController implements Initializable {
     private TableView<Sala> tabSalas;
     @FXML
     private TableView<Usuario> tabUsuarios;
-    @FXML
-    private TableView<Mensagem> tabMensagem;
-    @FXML
-    private TableColumn<Mensagem, String> tabAutor;
-    @FXML
-    private TableColumn<Mensagem, String> tabConteudo;
+    
     @FXML
     private TableColumn<Sala, String> listaSalas;
     @FXML
@@ -75,15 +69,18 @@ public class ChatController implements Initializable {
     private Button enviarMensagem;
     @FXML
     private Button sairSala;
-
-    private LoginController infoLogin;// ATENÇÃO: PARA ACESSAR USUARIO LOGADO USAR infoLogin.usuario
+    private LoginController infoLogin;
+    // ATENÇÃO: PARA ACESSAR USUARIO LOGADO USAR infoLogin.usuario
     private Client run;
     private Proxy proxy = new ProxyImpl();
     private Sala sala;
-    ManterUsuario manterUsuario = new ManterUsuarioImpl(new UsuarioDAOImpl());
-    ManterSala manterSala = new ManterSalaImpl(new SalaDAOImpl());
-    ManterMensagem manterMensagem = new ManterMensagemImpl(new MensagemDAOImpl());
 
+    ManterUsuario manterusuario = new ManterUsuarioImpl(new UsuarioDAOImpl());
+    ManterSala mantersala = new ManterSalaImpl(new SalaDAOImpl());
+    ManterMensagem mantermensagem = new ManterMensagemImpl(new MensagemDAOImpl());
+    @FXML
+    private ListView<String> listaMensagens;
+    
     public void setRun(Client run) {
         this.run = run;
     }
@@ -91,40 +88,38 @@ public class ChatController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         try {
-            atualizarSalas(FXCollections.observableArrayList(manterSala.getAll()));
-        } catch (PersistenceException ex) {
+            atualizarSalas(FXCollections.observableArrayList(mantersala.getAll()));
+            
+        } catch (PersistenceException ex ) {
             Logger.getLogger(ChatController.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        } 
     }
 
     @FXML
-    public void criarSala(ActionEvent event) throws PersistenceException, InterruptedException, BusinessException {
+    public void criarSala(ActionEvent event) throws PersistenceException {
         Sala novaSala = new Sala();
         novaSala.setNome(procuraSala.getText());
         LoginController.usuario.setSala(novaSala);
+        novaSala.addUsuario(LoginController.usuario);
         proxy.criarSala(novaSala);
         proxy.sairSala(LoginController.usuario);
         proxy.entrarSala(novaSala, LoginController.usuario);
         sala = novaSala;
-        Thread.sleep(100);
-        atualizarSalas(FXCollections.observableArrayList(manterSala.getAll()));
-        atualizarUsuarios(FXCollections.observableArrayList(manterUsuario.getAllByRoom(sala)));
+        
     }
 
     @FXML
-    public void entrarSala(ActionEvent event) throws PersistenceException, BusinessException, InterruptedException {
+    public void entrarSala(ActionEvent event) throws PersistenceException {
         System.out.println("Entrar sala");
         SalaDAO salaDAO = new SalaDAOImpl();
         sala = salaDAO.getSalaByNome(procuraSala.getText());
         System.out.println(sala == null ? "sala n existe" : "existe");
         LoginController.usuario.setSala(sala);
         proxy.entrarSala(sala, LoginController.usuario);
-        Thread.sleep(100);
-        atualizarUsuarios(FXCollections.observableArrayList(manterUsuario.getAllByRoom(sala)));
     }
 
     @FXML
-    public void enviarMensagem(ActionEvent event) throws PersistenceException, BusinessException, InterruptedException {
+    public void enviarMensagem(ActionEvent event) throws PersistenceException {
         System.out.println("Envia msg");
         String msg = textoEscrito.getText();
         Mensagem m = new Mensagem();
@@ -132,8 +127,7 @@ public class ChatController implements Initializable {
         m.setSala(sala);
         m.setAutor(LoginController.usuario);
         proxy.enviarMensagem(m);
-        Thread.sleep(100);
-        atualizarMensagens(FXCollections.observableArrayList(manterMensagem.getAllBySala(sala)));
+       
     }
 
     @FXML
@@ -142,8 +136,7 @@ public class ChatController implements Initializable {
         LoginController.usuario.setSala(null);
         proxy.sairSala(LoginController.usuario);
         sala = null;
-        atualizarMensagens(FXCollections.observableArrayList((Mensagem) null));
-        atualizarUsuarios(FXCollections.observableArrayList((Usuario) null));
+      
     }
 
     public void atualizarSalas(ObservableList<Sala> listSala) {
@@ -157,8 +150,18 @@ public class ChatController implements Initializable {
     }
 
     public void atualizarMensagens(ObservableList<Mensagem> listMensagem) {
-        tabAutor.setCellValueFactory(new PropertyValueFactory<Mensagem, String>("getAutor().getNome()")); // PROBLEMA AQUI
-        tabConteudo.setCellValueFactory(new PropertyValueFactory<Mensagem, String>("conteudo"));
-        tabMensagem.setItems(listMensagem);
+           
+        ObservableList<String> lista = FXCollections.observableArrayList();
+        for (int i=0; i<listMensagem.size(); i++){
+            lista.add(listMensagem.get(i).getAutor().getNome()+" disse: " + listMensagem.get(i).getConteudo());
+        }
+        
+        listaMensagens.setItems(lista);
     }
+    
+    
+        
+        
+    
+    
 }
